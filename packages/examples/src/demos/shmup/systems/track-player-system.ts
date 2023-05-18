c54f21e37beb562dca60822fd94bf6059406b9c8
Jake Klassen <jklassendev@gmail.com>
2023-05-18 18:57:26 -0400

    refactor some naming in a track player system

--- packages/examples/src/demos/shmup/systems/track-player-system.ts
+++ packages/examples/src/demos/shmup/systems/track-player-system.ts
@@ -5,16 +5,16 @@ import { Entity } from '../entity.ts';
  * Used for the player thruster to follow the player
  */
 export function trackPlayerSystemFactory({ world }: { world: World<Entity> }) {
-  const movables = world.archetype('trackPlayer', 'transform');
+  const playerChildren = world.archetype('trackPlayer', 'transform');
   const players = world.archetype('tagPlayer', 'transform');
 
   return function trackPlayerSystem() {
     for (const player of players.entities) {
-      for (const entity of movables.entities) {
-        entity.transform.position.x =
-          player.transform.position.x + (entity.trackPlayer.offset?.x ?? 0);
-        entity.transform.position.y =
-          player.transform.position.y + (entity.trackPlayer.offset?.y ?? 0);
+      for (const child of playerChildren.entities) {
+        child.transform.position.x =
+          player.transform.position.x + (child.trackPlayer.offset?.x ?? 0);
+        child.transform.position.y =
+          player.transform.position.y + (child.trackPlayer.offset?.y ?? 0);
       }
     }
   };
