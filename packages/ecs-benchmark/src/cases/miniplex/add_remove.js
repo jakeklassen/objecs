05cc4f8a75a03ca192ab6a282900b5d49376a422
Jake Klassen <jklassendev@gmail.com>
2023-05-19 09:18:23 -0400

    ecs benchmark

---
+++ packages/ecs-benchmark/src/cases/miniplex/add_remove.js
@@ -0,0 +1,20 @@
+// @ts-check
+import { World } from "miniplex";
+
+export default async (count) => {
+  const ecs = new World();
+
+  for (let i = 0; i < count; i++) {
+    ecs.createEntity({ A: true });
+  }
+
+  return () => {
+    for (const entity of ecs.entities) {
+      ecs.addComponent(entity, "B", true);
+    }
+
+    for (const entity of ecs.entities) {
+      ecs.removeComponent(entity, "B");
+    }
+  };
+};
