20978cf5b9bbb02ab0fe278d3882e775fbfeb525
Jake Klassen <jklassendev@gmail.com>
2025-01-02 16:00:18 -0500

    adjust type within shmup example

--- packages/examples/src/demos/shmup/entity/assert.ts
+++ packages/examples/src/demos/shmup/entity/assert.ts
@@ -1,17 +1,16 @@
-import { SetRequired } from "type-fest";
 import { Entity } from "../entity.ts";
 
-export function assertEnityHas<T extends Entity>(
+function assertEnityHas<T extends Entity>(
 	entity: T,
 	...components: Array<keyof T>
-): entity is SetRequired<T, (typeof components)[number]> {
+): entity is T & Required<Pick<T, (typeof components)[number]>> {
 	return components.every((component) => component in entity);
 }
 
 export function assertEnityHasOrThrow<T extends Entity>(
 	entity: T,
 	...components: Array<keyof T>
-): asserts entity is SetRequired<T, (typeof components)[number]> {
+): asserts entity is T & Required<Pick<T, (typeof components)[number]>> {
 	if (!assertEnityHas(entity, ...components)) {
 		throw new Error(`Entity is missing components: ${components.join(", ")}`);
 	}
